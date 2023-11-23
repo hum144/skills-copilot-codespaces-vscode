7bb5dc5739f5c40e104179fd1dc0032e345005fc
Humi144 <52263585+hum144@users.noreply.github.com>
2023-11-23 16:02:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,50 +1,48 @@
 // create web server
+// listen to port 3000
+// create a route for GET /comments
+// send back some comments
+// create a route for POST /comments
+// get the data from the body
+// push the data into the comments array
+// send back the comments array
+// create a route for DELETE /comments/:id
+// get the id from the params
+// remove the comment from the array
+// send back the comments array
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
 const cors = require('cors');
-// const { default: axios } = require('axios');
 
 const app = express();
+
+const comments = [
+    { id: 1, username: 'toto', comment: 'first comment' },
+    { id: 2, username: 'titi', comment: 'second comment' },
+    { id: 3, username: 'tata', comment: 'third comment' },
+];
+
 app.use(bodyParser.json());
 app.use(cors());
 
-const commentsByPostId = {};
-
-// get comments
-app.get('/posts/:id/comments', (req, res) => {
-    res.send(commentsByPostId[req.params.id] || []);
+app.get('/comments', (req, res) => {
+    res.send(comments);
 });
 
-// create comment
-app.post('/posts/:id/comments', (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
-    const { content } = req.body;
-    const comments = commentsByPostId[req.params.id] || [];
-    comments.push({ id: commentId, content });
-    commentsByPostId[req.params.id] = comments;
-
-    // send event to event bus
-    // await axios.post('http://localhost:4005/events', {
-    //     type: 'CommentCreated',
-    //     data: {
-    //         id: commentId,
-    //         content,
-    //         postId: req.params.id
-    //     }
-    // });
-
-    res.status(201).send(comments);
+app.post('/comments', (req, res) => {
+    const comment = req.body;
+    comments.push(comment);
+    res.send(comments);
 });
 
-// create event
-app.post('/events', (req, res) => {
-    console.log('Event Received:', req.body.type);
-
-    res.send({});
+app.delete('/comments/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const index = comments.findIndex((comment) => comment.id === id);
+    comments.splice(index, 1);
+    res.send(comments);
 });
 
-app.listen(4001, () => {
-    console.log('Listening on 4001');
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
 });
